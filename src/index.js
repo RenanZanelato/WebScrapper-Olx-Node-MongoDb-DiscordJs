9d6a3036732599beb2ff90bf67add1029caabf11
Renan Zanelato <renan.zanelato96@gmail.com>
2022-06-08 20:40:52 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -7,4 +7,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+
 client.login(token);
