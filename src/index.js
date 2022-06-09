0663e258716a688415e8f2ff51ec1e5b6d8a6559
Renan Zanelato <renan.zanelato96@gmail.com>
2022-06-08 21:39:31 -0300

    Created Discord bot to read olx

--- src/index.js
+++ src/index.js
@@ -1,25 +1,71 @@
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { MessageEmbed, WebhookClient } = require('discord.js');
+const { discordWebHook, targetSite } = require('./config.json');
+const { botName, avatar } = require('./discordBotConfig.json');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const webhookClient = new WebhookClient({ url: discordWebHook });
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+axios(targetSite)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const adsTable = $('.sc-1fcmfeb-2');
+    const ads = [];
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+    adsTable.each(function() {
+      const olx_id = $(this)
+        .find('a')
+        .attr('data-lurker_list_id');
 
-	const { commandName } = interaction;
+      const title = $(this)
+        .find('a')
+        .attr('title');
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
-});
+      const link = $(this)
+        .find('a')
+        .attr('href');
 
+      const img_link = $(this)
+        .find('img')
+        .attr('data-src');
 
-client.login(token);
+      const price = $(this)
+        .find('.sc-1kn4z61-1 span')
+        .text();
+
+      const locale = $(this)
+        .find('.sc-1c3ysll-0 span')
+        .text();
+
+      const created_date = $(this)
+        .find('.sc-11h4wdr-0')
+        .text();
+
+      if (typeof title === 'undefined'){
+      	return;	
+      }
+
+     const exampleEmbed = new MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle(title)
+		.setURL(link)
+		.setThumbnail(img_link)
+		.addFields(
+			{ name: 'Price', value: price },
+			{ name: 'Locale', value: locale},
+			{ name: 'Created Date', value: created_date }
+		)
+		.setImage(img_link)
+		.setTimestamp();
+
+		webhookClient.send({
+			username: botName,
+			avatarURL: avatar,
+			embeds: [exampleEmbed],
+		});
+
+    });
+  })
+  .catch(console.error);
